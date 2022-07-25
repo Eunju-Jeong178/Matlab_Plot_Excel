1acebebd29c29421be378baad2e42423d40ed49e
Eunju Jeong <77608922+Eunju-Jeong178@users.noreply.github.com>
2022-07-25 13:44:03 +0900

    Add files via upload

---
+++ readExcel.m
@@ -0,0 +1,9 @@
+[num,txt,raw] = xlsread("accuracy_of_ToF.xlsx");  % From the help
+x = num(:, 1);
+y = num(:, 2); 
+plot(x,y,'b-',LineWidth=1.5);
+box off
+xlabel('Distance by ruler [cm]','FontSize',15,'fontname','times new roman') 
+ylabel('Absolute error [cm]','FontSize',15,'fontname','times new roman')
+set(gcf,'Color','w')
+set(gca,'FontSize',13)
